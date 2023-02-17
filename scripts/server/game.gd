8a70edfa7e49c79f106ceb7999bc081a84dbf147
Caio Caldeira <caio.caldeira@most.com.br>
2023-02-17 11:26:54 -0300

    Add logs over server methods

--- scripts/server/game.gd
+++ scripts/server/game.gd
@@ -3,29 +3,35 @@ extends Node
 
 const LobbyInfo = preload("res://resources/LobbyInfo.gd")
 
-remote func position_guess (pid: int, guess_degrees: float):
+remote func position_guess (pid: int, guess_degree: float):
+	print("%d has positioned a guess" % pid, " at %f" % guess_degree)
 	var lobby: LobbyInfo = Lobby.get_lobby(pid)
 	if lobby == null or not lobby.is_guesser(pid):
+		print("%d is a invalid guesser" % pid)
 		return
 
 	Lobby.syncronize_lobby(
 		pid, lobby, "position_guess", { 
-			"guess_degrees": guess_degrees, "player_id": pid
+			"guess_degree": guess_degree, "player_id": pid
 		}
 	)
 
 remote func ready_guess (pid: int, guess_degree: float):
+	print("%d has finished guessing" % pid, " at %f" % guess_degree)
+
 	var lobby: LobbyInfo = Lobby.get_lobby(pid)
 	if lobby == null or not lobby.ready_guesser(pid, guess_degree):
+		print("%d is a invalid guesser" % pid)
 		return
 	
 	Lobby.syncronize_lobby(
 		pid, lobby, "ready_guess", {
-			"guess_degrees": guess_degree, "player_id": pid
+			"guess_degree": guess_degree, "player_id": pid
 		}
 	)
 
 	if lobby.all_ready():
+		print("compute scoring on players clients")
 		lobby.status = LobbyInfo.SCORING
 		Lobby.syncronize_lobby(
 			1, lobby, "all_ready", lobby.curr_round.score_degree
@@ -33,13 +39,18 @@ remote func ready_guess (pid: int, guess_degree: float):
 
 remote func update_score (pid: int, score: int):
 	var lobby: LobbyInfo = Lobby.get_lobby(pid)
-	if lobby != null:
-		lobby.update_score(pid, score)
+	if lobby == null or not lobby.update_score(pid, score):
+		print("lobby not found or invalid scorer")
+		return
 		
-		if lobby.all_scored():
-			lobby.status = LobbyInfo.WAITING
-			yield(get_tree().create_timer(5.0), "timeout")
-		
-			Lobby.syncronize_lobby(1, lobby.game_id, "start_round", {
-				"hinter_id": lobby.start_round()
-			})
+	print("%d marked a score of " % pid, "%d on this round" % score)
+	
+	if lobby.all_scored():
+		print("all round players have finished scoring phase")
+		print("start new round")
+		lobby.status = LobbyInfo.WAITING
+		yield(get_tree().create_timer(5.0), "timeout")
+	
+		Lobby.syncronize_lobby(1, lobby.game_id, "start_round", {
+			"hinter_id": lobby.start_round()
+		})
--- scripts/server/lobby.gd
+++ scripts/server/lobby.gd
@@ -7,20 +7,29 @@ enum { LOBBY, HINTING, GUESSING, SCORING, WAITING }
 var _open_lobbies: Dictionary = {}
 
 func signal_to_lobby (source_id: int, lobby: LobbyInfo, rpc_method: String):
+	print(
+		"signalling %d call " % source_id, rpc_method, " over lobby ", lobby.game_id
+	)
 	for pid in lobby.players_ids:
 		if source_id != pid:
+			print("signaling %d about " % pid, rpc_method)
 			rpc_id(pid, rpc_method)
 
-func syncronize_lobby (
+func synchronize_lobby (
 	source_id: int, lobby: LobbyInfo, rpc_method: String, data: Dictionary
 ):
+	print(
+		"synchronizing %d call " % source_id, rpc_method, " over lobby ", lobby.game_id
+	)
 	for pid in lobby.players_ids:
 		if source_id != pid:
+			print("synchronizing %d about " % pid, rpc_method)
 			rpc_id(pid, rpc_method, data)
 
 func get_lobby (pid: int):
 	var gid = Server.ONLINE_PLAYERS.get(pid, null)
 	if gid == null:
+		print("lobby not found")
 		return null
 
 	return _open_lobbies.get(gid, null)
@@ -32,54 +41,63 @@ func random_word ():
 
 	return word
 
-remote func valid_lobby (id: int):
-	print("Fetching Lobby name for %d" % id)
+func valid_lobby ():
 	Server.rng.randomize()
+	print("randomizing lobby id")
 
 	var word = random_word()
-	while word in _open_lobbies.keys():
+	while _open_lobbies.get(word, null) != null:
 		print("%s is taken" % word)
 		word = random_word()
 
 	print("chosen word is: %s" % word)
-	rpc_id(id, "valid_lobby", word)
+	return word
 
-remote func create_lobby (pid: int, gid: String, pname: String):
-	var success = false
-	if _open_lobbies.get(gid, null) != null:
-		_open_lobbies[gid] = LobbyInfo.new(gid, pid, pname)
-		success = true
+remote func create_lobby (pid: int, pname: String):
+	print("%d is creating a lobby" % pid)
+	var gid = valid_lobby()
 
-	rpc_id(pid, "created_lobby", success)
+	_open_lobbies[gid] = LobbyInfo.new(gid, pid, pname)
+	rpc_id(pid, "created_lobby", gid)
 
 remote func join_lobby (pid: int, gid: String, pname: String):
+	print("%d wants to join lobby " % pid, gid)
 	var lobby: LobbyInfo = _open_lobbies.get(gid, null)
-	var success = false
 
-	if lobby != null and lobby.add_player(pid, pname):
-		signal_to_lobby(pid, lobby, "join_lobby")
-		success = true
+	if lobby == null or not lobby.add_player(pid, pname):
+		print("lobby non-existent or full")
 
+		rpc_id(pid, "join_lobby", { "status": "error" })
+		return
+
+	signal_to_lobby(pid, lobby, "join_lobby")
 	rpc_id(pid, "joined_lobby", lobby.get_lobby_info())
 
 	if lobby.status == LobbyInfo.WAITING:
+		print("initializing game if it is the first player")
 		yield(get_tree().create_timer(5.0), "timeout")
 
-		syncronize_lobby(1, lobby.game_id, "start_round", {
+		synchronize_lobby(1, lobby.game_id, "start_round", {
 			"hinter_id": lobby.start_round()
 		})
 
 func remove_from_lobby (pid: int):
 	var lobby: LobbyInfo = get_lobby(pid)
+	print("remove %d from lobby " % pid, lobby.game_id)
 
 	Server.ONLINE_PLAYERS.erase(pid)
 	if lobby != null and lobby.remove_player(pid):
+		print("%d removed" % pid)
 		if lobby.is_empty():
+			print("deleting %s empty lobby")
 			_open_lobbies.erase(lobby.game_id)
 
 		else:
-			syncronize_lobby(pid, lobby, "exit_lobby", { "id": pid })
+			print(
+				"synchronize with online players that %d left the lobby" % pid
+			)
+			synchronize_lobby(pid, lobby, "exit_lobby", { "id": pid })
 
 remote func exit_lobby (id: int):
 	remove_from_lobby(id)
-	rpc_id(id, "exited_lobby", true)
+	rpc_id(id, "exited_lobby")
